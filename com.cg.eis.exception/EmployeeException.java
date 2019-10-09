7f94f596b3c35f7d58b59c2c8d6acecd03a5e128
rupinder-tech <55372758+rupinder-tech@users.noreply.github.com>
2019-10-09 21:23:31 +0530

    Delete EmployeeException.java

--- com.cg.eis.exception/EmployeeException.java
+++
@@ -1,8 +0,0 @@
-package com.cg.eis.exception;
-
-public class EmployeeException extends Exception {
-	
-	public void myException(){
-		System.out.println("The Salary of Employee should be above 3000 ");
-	}
-}
